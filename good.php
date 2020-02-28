61fc99f46b36636a264d100d56c5f8d0cffb70d7
yuya-konkon <yuuuuya08@icloud.com>
2020-02-28 10:23:38 +0900

    add connect good

--- good.php
+++ good.php
@@ -5,25 +5,23 @@ require_once('functions.php');
 
 $dbh = connectDb();
 
-// var_dump($_SERVER);
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
   // フォームに入力されたデータの受け取り
   $good = $_GET['good'];
-
   if ($good == "1") {
     $good_value = 1;
+    echo '1';
   } else {
     $good_value = 0;
+    echo '0';
   }
 
-  $sql = "update tweets set good = :good where id = :id";
   $stmt = $dbh->prepare($sql);
   $stmt->bindParam(':id', $id);
-  $stmt->bindParam(':good',$good);
   $stmt->execute();
   $tweets = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
-  header('Location: index.php');
-  exit;
+  // header('Location: index.php');
+  // exit;
 }
