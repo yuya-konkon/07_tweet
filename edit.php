c3f550f477c7a3c241e9f4fc188ee9e89ccdbc58
yuya-konkon <yuuuuya08@icloud.com>
2020-03-03 09:15:01 +0900

    q edit

--- edit.php
+++ edit.php
@@ -6,45 +6,36 @@ require_once('functions.php');
 $id = $_GET['id'];
 
 $dbh = connectDb();
-
 $sql = "select * from tweets where id = :id";
 $stmt = $dbh->prepare($sql);
-$stmt->bindParam("id", $id);
+$stmt->bindParam(":id", $id);
 $stmt->execute();
-
 $tweet = $stmt->fetch(PDO::FETCH_ASSOC);
 
 if (!$tweet) {
-  header('Location: index,php');
+  header('Location: index.php');
   exit;
 }
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $content = $_POST['content'];
   $errors = [];
-
-if ($content == '') {
-  $errors['content'] = '本文が未入力です。';
-}
-
-if ($content == $tweet['content']) {
-  $errors['content'] = "本文が変更されていません";
-}
-
-  if ($title == '') {
-    $errors['title'] = 'タスク名を入力してください。';
+  
+  // $contentの中身がなにもない状態になってしまっている
+  var_dump($content);
+  if ($content == '') {
+    $errors['content'] = '本文を入力してください。';
   }
 
-  if ($title == $plan['title']) {
-    $errors['title'] = 'タスク名が変更されていません。';
+  if($content = $tweet['content']) {
+    $errors['content'] = '本文が変更されていません。';
   }
 
-  if (empty($errors)) {
+  if(empty($errors)) {
     $dbh = connectDb();
-    $sql = "update tweets set content = :content, created_at = now() where id = :id";
+    $sql = "update tweets set content = :content, created_at = now() where id = ;id";
     $stmt = $dbh->prepare($sql);
-    $stmt->bindParam(':content',$content);
-    $stmt->bindParam(':id', $id);
+    $stmt->bindParam(":id", $id);
+    $stmt->bindParam(":content", $content);
     $stmt->execute();
 
     header('Location: index.php');
@@ -60,26 +51,26 @@ if ($content == $tweet['content']) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>編集画面</title>
+  <title>編集</title>
   <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
   <h1>tweetの編集</h1>
   <a href="index.php">戻る</a>
-  <?php if ($errors) :?>
+  <?php if ($errors) : ?>
     <ul class="error-list">
-      <?php foreach ($errors as $error) :?>
-      <li>
-        <?php echo h($error) ; ?>
-      </li>
+      <?php foreach ($errors as $error) : ?>
+        <li>
+          <?php echo h($error); ?>
+        </li>
       <?php endforeach; ?>
     </ul>
-  <?php endif ; ?>
+  <?php endif; ?>
   <form action="" method="post">
-    <label for="body">本文</label><br>
-    <textarea name="body" id="" cols="30" rows="5" placeholder=""><?php echo h($tweet['content']); ?></textarea><br>
-    <input type="submit" value="編集する">
+    <p><label for="content">本文</label></p>
+    <textarea name="content" cols="30" rows="5"><?php echo h($tweet['content']);?></textarea>
+    <p><input type="submit" value="編集"></p>
   </form>
 </body>
 
